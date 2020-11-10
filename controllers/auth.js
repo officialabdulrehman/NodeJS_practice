cb8dd79021386edaf633de0b8630fdfc1b4a17a3
officialabdulrehman <arehman08@hotmail.com>
2020-11-10 11:44:09 +0500

    r

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,6 @@
 const crypto = require('crypto');
 
 const bcrypt = require('bcryptjs');
-const nodemailer = require('nodemailer');
-const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
 const key = require('../sensitive');
@@ -81,14 +79,7 @@ exports.postSignup = (req, res, next) => {
       })
       .then(result => {
         res.redirect('/login')
-        return transporter.sendMail({
-          to: email,
-          from: 'arehman08@hotmail.com',
-          subject: 'Signup succeeded',
-          html: '<h1>You successfully signed up!</h1>'
-        })
       })
-      .catch(err => console.log(err))
     })
     .catch(err => console.log(err))
 };
