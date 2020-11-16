aa58e28801af286dfbee5cb17f2244fe3c502137
officialabdulrehman <arehman08@hotmail.com>
2020-11-16 14:51:42 +0500

    Added some client-side js for deleting element and updating DOM with redirect/res

--- controllers/admin.js
+++ controllers/admin.js
@@ -152,8 +152,8 @@ exports.getProducts = (req, res, next) => {
   })
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then(product => {
       if(!product){
@@ -164,12 +164,14 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .then(result => {
       console.log('Deleted')
-      res.redirect('/admin/products')
+      res.status(200).json({
+        message: 'Success!'
+      })
     })
     .catch((err) => {
       console.log(err)
-      const error = new Error(err)
-      error.httpStatusCode = 500
-      return next(error)
+      res.status(500).json({
+        message: 'Deleting operation failed'
+      })
     })
 }
